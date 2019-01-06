fd957160a22554ba7caa96feba98f0f470b8730d
Nathanael-the-real <nat.dumar@gmail.com>
2019-01-06 22:40:09 +0100

    modified styles.js, added 2 features to avoid gulp crash.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,5 +9,9 @@ var gulp = require('gulp'),
 gulp.task('styles', function () {
   return gulp.src('./app/assets/styles/style.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .on('error', function (errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
